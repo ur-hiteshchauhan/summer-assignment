9df91e7446b43a188230959022b4a76997cc575d
Hitesh Chauhan <hiteshworks01@gmail.com>
2026-06-20 08:49:05 +0530

    Day-11 assignment is uploaded

---
+++ Day_11/Q_43.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int isPrime(int n)
+{
+    if(n <= 1)
+        return 0;
+
+    for(int i = 2; i < n; i++)
+    {
+        if(n % i == 0)
+            return 0;
+    }
+
+    return 1;
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if(isPrime(n))
+        printf("Prime Number");
+    else
+        printf("Not Prime Number");
+
+    return 0;
+}
